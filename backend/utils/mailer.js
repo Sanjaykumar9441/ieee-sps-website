acaa6c4950bdefd0e706dd3130c3ea9dd0b8e066
Sanjay Kumar Chitturi <sanjaykumarchitturi@gmail.com>
2026-03-06 21:53:02 +0530

    add brevo mail system

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,35 +1,35 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,           // ✅ use 587 instead of 465
-  secure: false,       // TLS
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
 const sendMail = async (to, subject, html) => {
+
   try {
 
     await transporter.sendMail({
-      from: `"Arduino Days 2026" <${process.env.EMAIL_USER}>`,
+      from: '"Arduino Days 2026" <noreply@arduino-days.com>',
       to,
       subject,
       html
     });
 
-    console.log("✅ Email sent:", to);
+    console.log("Email sent:", to);
 
   } catch (error) {
 
     console.error("Mail error:", error);
 
   }
+
 };
 
 module.exports = sendMail;
