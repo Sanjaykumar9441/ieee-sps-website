7890b08f2ddbaab848d298ff925fd0c00ab1bf37
Sanjay Kumar Chitturi <sanjaykumarchitturi@gmail.com>
2026-07-25 23:45:21 +0530

    Install Chrome for Puppeteer

--- backend/pdf/generateAcknowledgement.js
+++ backend/pdf/generateAcknowledgement.js
@@ -174,8 +174,12 @@ const generateAcknowledgement = async (registration) => {
     .replace(/{{qrCode}}/g, qrCode);
 
   const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ],
   });
 
   const page = await browser.newPage();
