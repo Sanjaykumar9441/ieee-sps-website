73a158e02ef47e69b99d7db661c5bed23a4059d4
Sanjay Kumar Chitturi <sanjaykumarchitturi@gmail.com>
2026-07-26 21:32:32 +0530

    checkk

--- frontend/src/api/spaceDayAdmin.ts
+++ frontend/src/api/spaceDayAdmin.ts
@@ -4,11 +4,18 @@ export const updatePaymentStatus = async (
   registrationId: string,
   paymentStatus: "Verified" | "Rejected",
 ) => {
+  const token = localStorage.getItem("token");
+
   const res = await axios.put(
     `${import.meta.env.VITE_API_URL}/api/space-day/admin/payment/${registrationId}`,
     {
       paymentStatus,
     },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
   );
 
   return res.data;
