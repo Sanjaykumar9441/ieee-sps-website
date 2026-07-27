bfaf9e9d1456edd1c3daa3518072d93371579b0c
Sanjay Kumar Chitturi <sanjaykumarchitturi@gmail.com>
2026-07-27 20:58:59 +0530

    cleared socket backend issue

--- backend/server.js
+++ backend/server.js
@@ -155,12 +155,6 @@ const PORT = process.env.PORT;
 
 console.log("🚀 Starting server...");
 
-// ✅ START SERVER IMMEDIATELY
-const http = require("http");
-const { Server } = require("socket.io");
-
-const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://ieeespsaditya.vercel.app"],
@@ -168,7 +162,7 @@ const io = new Server(server, {
   },
 });
 
-app.set("io", io);
+initSocket(io);
 
 io.on("connection", (socket) => {
   console.log("🟢 Admin Connected:", socket.id);
