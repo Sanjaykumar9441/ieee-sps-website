c477e457ab84b7fe6358513f14556432cd145c3d
Sanjay Kumar Chitturi <sanjaykumarchitturi@gmail.com>
2026-08-31 08:08:03 +0530

    Remove disqualify route from assessment admin API

--- backend/routes/adminForceSubmitRoutes.js
+++ backend/routes/adminForceSubmitRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
 const verifyToken = require("../middleware/verifyToken");
 const controller = require("../controllers/adminForceSubmitController");
 
 router.post("/:attemptId", verifyToken, controller.forceSubmit);
 router.post("/assessment/:assessmentId", verifyToken, controller.forceSubmitAll);
-router.post("/:attemptId/disqualify", verifyToken, controller.disqualify);
 
 module.exports = router;
